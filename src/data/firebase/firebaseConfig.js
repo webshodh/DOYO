c57bed2cf1d4fb1e3412f6a8a5b48b04ad40d702
VISHAL VIJAY GHOLKAR <49200013+Vishal7674@users.noreply.github.com>
2024-08-11 20:42:58 +0530

    firebaseconfig change

--- src/data/firebase/firebaseConfig.js
+++ src/data/firebase/firebaseConfig.js
@@ -3,15 +3,26 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import { getDatabase, ref, push, set, serverTimestamp, onValue, update, remove} from "firebase/database";
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCO8cKH_pomhLcbT1sO2H4QWoD4Z6GrxAE",
+  authDomain: "menu-app-1332d.firebaseapp.com",
+  databaseURL: "https://menu-app-1332d-default-rtdb.firebaseio.com",
+  projectId: "menu-app-1332d",
+  storageBucket: "menu-app-1332d.appspot.com",
+  messagingSenderId: "247653223701",
+  appId: "1:247653223701:web:016dca75d85db9f7182d1c",
+  measurementId: "G-ZYJWWYWD65"
 };
 
 // Initialize Firebase
